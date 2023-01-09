e66e1806c9f95f6372a787a7345236250bcda0cf
Geena Jackson <geena.jackson96@gmail.com>
2023-01-09 10:50:05 -0600

    modify second digit of minutes to not include inital 0

--- timeWord.js
+++ timeWord.js
@@ -14,14 +14,17 @@ function timeWord(digits) {
 
     timePhrase += hours + " ";
 
-    if (time[1] >= "10" && time[1] <= "19") {
+    if (time[1] === "00") {
+        timePhrase += "o'clock ";
+    }
+    else if (time[1] >= "10" && time[1] <= "19") {
         timePhrase += minuteTens[time[1]] + " ";
     }
     else {
         timePhrase += `${minuteFirst[minutes[0]]} ${minuteSecond[minutes[1]]} `
     }
 
-    if (hours < "12") {
+    if (time[0] < "12") {
         timePhrase += "am";
     }
     else {
@@ -69,15 +72,15 @@ const minuteFirst = {
 }
 
 const minuteSecond = {
-    "01": "one",
-    "02": "two",
-    "03": "three",
-    "04": "four",
-    "05": "five",
-    "06": "six",
-    "07": "seven",
-    "08": "eight",
-    "09": "nine",
+    "1": "one",
+    "2": "two",
+    "3": "three",
+    "4": "four",
+    "5": "five",
+    "6": "six",
+    "7": "seven",
+    "8": "eight",
+    "9": "nine",
 }
 
 const minuteTens = {
@@ -92,5 +95,6 @@ const minuteTens = {
     "18": "eighteen",
     "19": "nineteen",
 }
+
 
 module.exports = timeWord;
