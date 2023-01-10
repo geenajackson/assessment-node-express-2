388a52855debe9d2d3f9f9d9935ef03ca761c5af
Geena Jackson <geena.jackson96@gmail.com>
2023-01-10 11:31:22 -0600

    fix bug 3 by adding throw keyword

--- bankly/models/user.js
+++ bankly/models/user.js
@@ -111,7 +111,8 @@ class User {
     const user = result.rows[0];
 
     if (!user) {
-      new ExpressError('No such user', 404);
+      //FIXES BUG #3
+      throw new ExpressError('No such user', 404);
     }
 
     return user;
